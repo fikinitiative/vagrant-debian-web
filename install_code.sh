ca3f387eb8d8cd550e593a147a05b2faf67ef377
Minaya <minaya@thefik.com>
2013-11-05 17:32:18 +0100

    Add sh script to get wp-fikstores project

---
+++ install_code.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Copy clean database production dump from dropbox to ~/Sites/sql/ddbb.sql
+
+DIR="/Users/$(whoami)/Sites/wp-fikstore1/"
+
+if [ -d "$DIR" ]; then
+    true
+else
+    echo "Creating $DIR..."
+    mkdir -p $DIR
+fi
+
+echo "Copying wp-fikstore project..."
+git clone git@github.com:fikinitiative/wp-fikstore.git $DIR
+cp "$DIR/wp-config.php.local" "$DIR/wp-config.php"
+
+echo "Done!"
