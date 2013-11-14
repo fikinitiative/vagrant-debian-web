4701c2056c7b52c335f89269976e3dd1a401d02d
minayaserrano <minayaserrano@gmail.com>
2013-11-14 11:49:25 +0100

    Add linux install scripts

---
+++ install_ddbb_linux.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Copy clean database production dump from dropbox to ~/Sites/sql/ddbb.sql
+
+DIR="/home/$(whoami)/Sites/sql/"
+DDBB="ddbb.sql"
+URL="https://dl.dropboxusercontent.com/s/k8ovlmpxc2x7lf7/wp-fikstores.latest.sql.gz"
+
+if [ -d "$DIR" ]; then
+    true
+else
+    echo "Creating $DIR..."
+    mkdir -p $DIR
+fi
+
+echo "Downloading database..."
+curl --output "$DDBB.gz" -OL# $URL
+wait
+gzip -d "$DDBB.gz"
+mv $DDBB "$DIR/$DDBB"
+
+echo "Done!"
