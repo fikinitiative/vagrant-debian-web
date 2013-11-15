2bdee35d073e5e353c9186d657980e2672353cf0
minayaserrano <minayaserrano@gmail.com>
2013-11-15 12:16:54 +0100

    NFS deactivate

--- install_code_linux.sh
+++ install_code_linux.sh
@@ -14,4 +14,18 @@ echo "Copying wp-fikstore project..."
 git clone git@github.com:fikinitiative/wp-fikstore.git $DIR
 cp "$DIR/wp-config.php.local" "$DIR/wp-config.php"
 
+echo "Init submodules..."
+cd $DIR
+git submodule init
+git submodule update
+
+echo "Download composer..."
+cd ..
+curl -sS https://getcomposer.org/installer | php
+
+echo "Composer install..."
+cd $DIR
+../composer.phar selfupdate
+../composer.phar install
+
 echo "Done!"
---
+++ install_linux.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Install fistores wp environment on linux
+
+bash install_ddbb_linux.sh
+bash install_code_linux.sh
+
+vagrant up
